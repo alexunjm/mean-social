3d002ee3ac279404a203fb431c8b27745593b406
alexun <alexunjm@gmail.com>
2018-01-23 10:12:28 -0500

    middleware y headers de autenticación

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,12 +1,14 @@
 'use strict'
 
 var express = require('express');
+
 var UserController = require('../controllers/user');
+var md_auth = require('../middlewares/authentication');
 
 var api = express.Router();
 
 api.get('/home', UserController.home);
-api.get('/test', UserController.test);
+api.get('/test', md_auth.ensureAuth, UserController.test);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
