26b4f2cbe5a3cdd9ba5daa2f8ccc635bc6249a2e
alexun <alexunjm@gmail.com>
2018-02-17 15:49:23 -0500

    template externo como html

--- sample/src/app/manual/manual.component.ts
+++ sample/src/app/manual/manual.component.ts
@@ -2,17 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'manual',
-  template: `
-
-    <h2>Componente hecho de forma manual</h2>
-    <h3>{{nombre}}</h3>
-    <h3 [style.background]="yellow" *ngIf="mostrarEjemplo">Ejemplo if en componente {{ejemplo}}</h3>
-    <button (click)="mostrarEjemplo = !mostrarEjemplo">toogle</button>
-    <h4>lista desde un array</h4>
-    <ul>
-      <li *ngFor="let sample of arraySample">{{ sample }}</li>
-    </ul>
-  `
+  templateUrl: './manual.component.html'
 })
 export class ManualComponent{
   public nombre = 'Componente';
